96134179b1fbfc3aa04d21ebdb859168b387b9b9
Bokyung Moon <elaine.moon.72@gmail.com>
2018-07-20 00:01:27 -0400

    update to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/bmoon4-angular-app'));
+app.use(express.static('/dist/my-app'));
 
-app.get('/*', function(req,res) {
+app.get('*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/index.html'));
+res.sendFile(path.join(__dirname+'/dist/my-app/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
