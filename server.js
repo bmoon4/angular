f26033e9d35bd9030874ef40515e29558836e1c8
Bokyung Moon <elaine.moon.72@gmail.com>
2018-07-20 01:38:34 -0400

    upload

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-// //Install express server
-// const express = require('express');
-// const path = require('path');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-// const app = express();
+const app = express();
 
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname+ '/dist/my-app'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname+ '/dist/my-app'));
 
-// app.get('*', function(req,res) {
+app.get('*', function(req,res) {
     
-// res.sendFile(path.join(__dirname+'/dist/my-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/my-app/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 3000);
+
+// var express = require('express');
+// var app     = express();
+// var path    = require('path');
+
+// // Angular
+// app.use(express.static(path.resolve(__dirname, '../dist'))); //1
+// app.get('*', function (req, res) { //2
+//   var indexFile = path.resolve(__dirname,'../dist/index.html');
+//   res.sendFile(indexFile);
 // });
 
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 3000);
-
-var express = require('express');
-var app     = express();
-var path    = require('path');
-
-// Angular
-app.use(express.static(path.resolve(__dirname, '../dist'))); //1
-app.get('*', function (req, res) { //2
-  var indexFile = path.resolve(__dirname,'../dist/index.html');
-  res.sendFile(indexFile);
-});
-
-// Server
-var port = process.env.PORT || 3000; //3
-app.listen(port, function(){
-  console.log('listening on port:' + port);
-});
+// // Server
+// var port = process.env.PORT || 3000; //3
+// app.listen(port, function(){
+//   console.log('listening on port:' + port);
+// });
